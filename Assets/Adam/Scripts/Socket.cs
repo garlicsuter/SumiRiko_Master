fe173388eea424e4924cea74af4ef8cefa7eeba4
Adam <adamjasoncalvelage@gmail.com>
2023-04-22 20:51:53 -0400

    Basic socket functionality is complete
    -Implemented tolerances for xyz axis
    -Improved workflow and readability
    -Cleanup

--- Assets/Adam/Scripts/Socket.cs
+++ Assets/Adam/Scripts/Socket.cs
@@ -1,91 +1,107 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-/// <summary>
-/// Written by Adam Calvelage | adamjasoncalvelage@gmail.com
-/// </summary>
+//
+// Written by Adam Calvelage -> adamjasoncalvelage@gmail.com
+//
+
 public class Socket : MonoBehaviour
 {
     private XRRayInteractor rightController, leftController;
 
-    private GenericObject child = new GenericObject();
-    private GenericObject clone = new GenericObject();
+    private GenericObject child, clone;
 
     private GameObject heldObject;
 
+    private float radius;
+    private float speed;
+
+
+    [Tooltip("Up/Down, Left/Right, Tilt")]
+    public Vector3 angles;
+
+    [Tooltip("Scale of the Trigger")]
+    public float size = 1.0f;
+
+    [Range(0.0f, 1.0f)]
+    [Tooltip("Slow - Near-Instant")]
+    public float speedRatio = 0.5f;
+
+    [Tooltip("Colorless Translucent Material")]
     public Material material;
 
-    // TODO: Make it ratio between 0 to 1 where radius impacts speed?
-    public float lerpSpeed = 5.0f;
-
-    public float radius = 0.5f;
-
-    // TODO: Tolerance for each axis
-    public float tolerance;
 
     private void Start()
     {
         rightController = GameObject.Find("LeftHand Controller").GetComponent<XRRayInteractor>();
         leftController = GameObject.Find("RightHand Controller").GetComponent<XRRayInteractor>();
 
+        radius = size * 0.5f;
+        speed = speedRatio > 0 ? speedRatio * 10 : 0.1f;
+
         var collider = gameObject.AddComponent<SphereCollider>();
         collider.hideFlags = HideFlags.HideInInspector;
         collider.isTrigger = true;
         collider.radius = radius;
     }
-
     private void Update()
     {
-        if (child.gameObject != null)
+        if(child.gameObject != null)
         {
-            if (!IsGrabbed(child.gameObject))
+            if(!IsGrabbed(child.gameObject))
             {
-                // Lerp child position to the center of the socket
+                // Lerp position to the center of the socket
                 float distance = Vector3.Distance(child.transform.position, transform.position);
-                child.transform.position = distance <= 0.001f ? transform.position : Vector3.Lerp(child.transform.position, transform.position, lerpSpeed * Time.deltaTime);
+                child.transform.position = distance <= 0.001f ? transform.position : Vector3.Lerp(child.transform.position, transform.position, speed * Time.deltaTime);
 
-                // Lerp child rotation to the forward direction of the socket
+                // Lerp rotation to the forward direction of the socket
                 float angle = Quaternion.Angle(child.transform.rotation, transform.rotation);
-                child.transform.rotation = angle <= 1 ? transform.rotation : Quaternion.Lerp(child.transform.rotation, transform.rotation, lerpSpeed * Time.deltaTime);
+                child.transform.rotation = angle <= 0.5f ? transform.rotation : Quaternion.Lerp(child.transform.rotation, transform.rotation, speed * Time.deltaTime);
             }
             else
             {
-                RemoveChild();
+                StartCoroutine(RemoveChild());
             }
         }
     }
-
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider collider)
     {
-        if (child.gameObject == null)
+        if(child.gameObject == null)
         {
-            heldObject = other.gameObject;
+            // [BUG REPORT][Visual] - AC
+            // Position two or more alternative invalid meshes, remove cloned object,
+            // cloned mesh is not altered until the current heldObject is removed.
+            // Requires some thought to fix but will never get noticed so...
 
-            // Create a clone of the held object as a visual aid
-            if (clone.gameObject == null)
+            // Any interactable that contacts this trigger
+            heldObject = collider.gameObject;
+
+            // Setup a clone of the held object
+            if(clone.gameObject == null)
             {
-                // Fetch values
+                // Create object and components
                 clone.gameObject = new GameObject("Clone");
                 clone.filter = clone.gameObject.AddComponent<MeshFilter>();
                 clone.renderer = clone.gameObject.AddComponent<MeshRenderer>();
 
-                // Copy values
+                // Copy values to the cloned object
                 clone.filter.mesh = heldObject.GetComponent<MeshFilter>().sharedMesh;
                 clone.renderer.material = material;
                 clone.transform.SetParent(transform);
-                clone.transform.position = transform.position;
-                clone.transform.rotation = transform.rotation;
+                clone.transform.SetPositionAndRotation(transform.position, transform.rotation);
                 clone.transform.localScale = heldObject.transform.localScale;
             }
 
             // Check for a valid orientation
-            if (IsValid(heldObject))
+            if(IsValid(heldObject))
             {
+                // Valid color
                 clone.color = Color.green;
 
                 // Release the object to confirm selection
-                if (!IsGrabbed(heldObject))
+                if(!IsGrabbed(heldObject))
                 {
                     child.gameObject = heldObject;
                     child.body = heldObject.GetComponent<Rigidbody>();
@@ -99,121 +115,133 @@ public class Socket : MonoBehaviour
             }
             else
             {
+                // Invalid color
                 clone.color = Color.red;
             }
         }
     }
-
     private void OnTriggerExit(Collider collider)
     {
         Vector3 point = collider.ClosestPoint(transform.position);
         float distance = Vector3.Distance(point, transform.position);
 
         // Remove visual
-        if (collider.gameObject == heldObject && distance >= radius)
+        if(collider.gameObject == heldObject && distance >= radius)
         {
             heldObject = null;
             clone.Destroy();
         }
-
-        if (collider.gameObject == child.gameObject && distance >= radius)
-        {
-            RemoveChild();
-        }
     }
-
-    /// <summary>
-    /// Does the user have this object in their hand?
-    /// </summary>
-    public bool IsGrabbed(GameObject obj)
-    {
-        if (rightController.hasSelection)
-        {
-            if (rightController.interactablesSelected[0].transform.gameObject == obj)
-            {
-                return true;
-            }
-        }
-        if (leftController.hasSelection)
-        {
-            if (leftController.interactablesSelected[0].transform.gameObject == obj)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// Does this object align with this socket?
-    /// </summary>
-    public bool IsValid(GameObject obj)
-    {
-        float angle = Quaternion.Angle(obj.transform.rotation, transform.rotation);
-        angle = Math.Abs(angle);
-        return angle <= tolerance;
-    }
-
-    /// <summary>
-    /// Remove and dereference the child
-    /// </summary>
-    private void RemoveChild()
-    {
-        // Wait until the user has dropped the child before resetting it
-        if (child.gameObject.GetComponent<SocketChildReset>() == null)
-        {
-            var comp = child.gameObject.AddComponent<SocketChildReset>();
-            var func = comp.Wait(this, child.gameObject);
-            StartCoroutine(func);
-        }
-
-        child.gameObject = null;
-        child.body = null;
-    }
-
     private void OnDrawGizmos()
     {
         // Draw a sphere
         Gizmos.color = Color.gray;
         Gizmos.DrawWireSphere(transform.position, radius);
 
-        // Draw an invisible sphere to allow selection in scene window
+        // Draw an invisible sphere to allow selection in the scene window
         Gizmos.color = Color.clear;
         Gizmos.DrawSphere(transform.position, radius);
     }
-
     private void OnDrawGizmosSelected()
     {
         // Draw an arrow
         Gizmos.color = Color.red;
         float length = radius * 1.5f;
         float arms = length * 0.25f;
-        Vector3 end = (transform.forward * length) + transform.position;
+        Vector3 end = transform.forward * length + transform.position;
         Gizmos.DrawLine(transform.position, end);
         Gizmos.DrawLine(end, (transform.right - transform.forward) * arms + end);
         Gizmos.DrawLine(end, (-transform.right - transform.forward) * arms + end);
     }
+    private void OnValidate()
+    {
+        radius = size * 0.5f;
+        speed = speedRatio > 0 ? speedRatio * 10 : 0.1f;
+
+        if(gameObject.TryGetComponent<SphereCollider>(out SphereCollider collider))
+        {
+            collider.radius = radius;
+        }
+    }
+
+
+    /// <summary>
+    /// Does the user have this object in their hand?
+    /// </summary>
+    private bool IsGrabbed(GameObject obj)
+    {
+        if(rightController.hasSelection)
+        {
+            if(rightController.interactablesSelected[0].transform.gameObject == obj) // How can one controller select multiple interactables anyway?
+            {
+                return true;
+            }
+        }
+        if(leftController.hasSelection)
+        {
+            if(leftController.interactablesSelected[0].transform.gameObject == obj)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    /// <summary>
+    /// Does this object fit within the tolerances?
+    /// </summary>
+    private bool IsValid(GameObject obj)
+    {
+        float x = Quaternion.Angle(Quaternion.Euler(obj.transform.eulerAngles.x, 0, 0), Quaternion.Euler(transform.eulerAngles.x, 0, 0));
+        float y = Quaternion.Angle(Quaternion.Euler(0, obj.transform.eulerAngles.y, 0), Quaternion.Euler(0, transform.eulerAngles.y, 0));
+        float z = Quaternion.Angle(Quaternion.Euler(0, 0, obj.transform.eulerAngles.z), Quaternion.Euler(0, 0, transform.eulerAngles.z));
+        return x <= angles.x && y <= angles.y && z <= angles.z;
+    }
+    /// <summary>
+    /// Dereference and reset the child
+    /// </summary>
+    private IEnumerator RemoveChild()
+    {
+        var obj = child.gameObject;
+
+        child.gameObject = null;
+        child.body = null;
+
+        // Wait until the user has dropped the child because
+        // the interactable component does not like this
+        while(IsGrabbed(obj))
+        {
+            yield return null;
+        }
+
+        obj.GetComponent<Rigidbody>().isKinematic = false;
+        obj.transform.parent = null;
+    }
 }
 
+
+/// <summary>
+/// An organized and modular collection of references
+/// </summary>
 [Serializable]
 internal struct GenericObject
 {
-    public GameObject gameObject;
-    public Rigidbody body;
-    public MeshFilter filter;
-    public MeshRenderer renderer;
+    internal GameObject gameObject;
+    internal Rigidbody body;
+    internal MeshFilter filter;
+    internal MeshRenderer renderer;
 
-    public Color color
+    internal Color color
     {
         get => renderer.material.color;
         set => renderer.material.color = value;
     }
-    public Transform transform
+
+    internal Transform transform
     {
         get => gameObject.transform;
     }
 
-    public void Destroy()
+    internal void Destroy()
     {
         UnityEngine.Object.Destroy(gameObject);
 
