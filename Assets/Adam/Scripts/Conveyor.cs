7d07a1ea79395fb6a4b0b35015a47a10ace03c05
Adam <adamjasoncalvelage@gmail.com>
2023-04-25 20:33:29 -0400

    Conveyor Experimentation
    Trying to get sockets evenly spread out procedurally is kind of tricky.

--- Assets/Adam/Scripts/Conveyor.cs
+++ Assets/Adam/Scripts/Conveyor.cs
@@ -3,40 +3,62 @@ using UnityEngine;
 
 public class Conveyor : MonoBehaviour
 {
-    [NonSerialized]
-    public Transform[] nodes;
+    private Node[] nodes;
+    private float length;
 
-    public float socketSpacing;
+    public float spacing;
 
-    private void Start()
-    {
-
-    }
-    private void Update()
-    {
-
-    }
     private void OnValidate()
     {
-        nodes = new Transform[transform.childCount];
+        // Reset value
+        length = 0.0f;
 
-        for (int i = 0; i < transform.childCount; i++)
+        // Init nodes
+        nodes = new Node[transform.childCount];
+        for(int i = 0; i < transform.childCount; i++)
         {
-            nodes[i] = transform.GetChild(i);
+            // Fetch current & next child
+            Transform child = transform.GetChild(i);
+            Transform next = ((i + 1) < transform.childCount) ? transform.GetChild(i + 1) : null;
+
+            // Calculate distance between children
+            float dist = next != null ? Vector3.Distance(child.position, next.position) : 0.0f;
+
+            // Set values
+            nodes[i].position = child.position;
+            nodes[i].length = dist;
+            length += dist;
         }
     }
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-
-        for (int i = 0; i < nodes.Length; i++)
+        for(int i = 0; i < nodes.Length; i++)
         {
+            Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(nodes[i].position, 0.1f);
 
-            if (i < nodes.Length - 1 && nodes[i + 1] != null)
+            // Does this node continue?
+            if(i + 1 < nodes.Length)
             {
+                Gizmos.color = Color.cyan;
                 Gizmos.DrawLine(nodes[i].position, nodes[i + 1].position);
+
+                Vector3 dir = (nodes[i + 1].position - nodes[i].position).normalized;
+                float total = (nodes[i].length / spacing);
+                for(int f = 1; f < (int)(total); f++)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireSphere(nodes[i].position + (dir * (nodes.Length / f)), 0.025f);
+                }
             }
         }
     }
+}
+
+[Serializable]
+internal struct Node
+{
+    internal Vector3 position;
+    internal float length;
+    internal bool end => length == 0;
 }
