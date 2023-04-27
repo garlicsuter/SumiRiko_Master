21d8fef2b2677a60cc171f88c94a616a1b5d786e
Adam <70708407+23calva@users.noreply.github.com>
2023-04-27 08:25:01 -0400

    Save

--- Assets/Adam/Scripts/Conveyor.cs
+++ Assets/Adam/Scripts/Conveyor.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using System.Linq;
 
 public class Conveyor : MonoBehaviour
 {
+    private GameObject[] clones;
     private _Transform[] points;
     private Node[] nodes;
     private float length;
@@ -17,67 +18,83 @@ public class Conveyor : MonoBehaviour
     {
         InitNodes();
 
-        foreach(var point in points)
+        points = GeneratePoints().ToArray();
+
+        for ( int i = 0; i < points.Length; i++)
         {
-            var obj = Instantiate(clone, transform);
-            obj.transform.position = point.position;
-            obj.transform.rotation = point.rotation;
+            clones[i] = Instantiate(clone, transform);
+            clones[i].transform.position = points[i].position;
+            clones[i].transform.rotation = points[i].rotation;
         }
     }
     private void Update()
     {
-
+        for( int i = 0; i < points.Length; i++)
+        {
+            clones[i].transform.position += points[i].forward * Time.deltaTime;
+        }
     }
     private void OnValidate()
     {
-        if(Application.isPlaying == false)
+        if (Application.isPlaying == false)
+        {
             InitNodes();
+            points = GeneratePoints().ToArray();
+        }
     }
     private void OnDrawGizmos()
     {
-        for(int i = 0; i < nodes.Length; i++)
+        for (int i = 0; i < nodes.Length; i++)
         {
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(nodes[i].position, 0.1f);
 
             // Does this node continue?
-            if(nodes[i].isEnd == false)
+            if (nodes[i].isEnd == false)
             {
                 Gizmos.color = Color.cyan;
                 Gizmos.DrawLine(nodes[i].position, nodes[i + 1].position);
             }
         }
 
-        for(int i = 0; i < points.Length; i++)
+        int count = 1;
+        foreach (var point in points)
         {
-            Handles.DrawWireCube(points[i].position, Vector3.one * 0.05f);
-            Handles.Label(points[i].position, (i + 1).ToString());
+            Handles.DrawWireCube(point.position, Vector3.one * 0.05f);
+            Handles.Label(point.position, (count++).ToString());
         }
     }
 
     private void InitNodes()
     {
-        // Reinitialize value just in case
+        // ...
         length = 0.0f;
-
-        // Init nodes
         nodes = new Node[transform.childCount];
-        for(int i = 0; i < transform.childCount; i++)
+
+        // ...
+        for (int i = 0; i < transform.childCount; i++)
         {
             // Fetch current & next child
             Transform child = transform.GetChild(i);
             Transform next = ((i + 1) < transform.childCount) ? transform.GetChild(i + 1) : null;
 
-            // Calculate distance between children
-            float dist = next != null ? Vector3.Distance(child.position, next.position) : 0.0f;
+            // ...
+            float distance = 0.0f;
+            Vector3 forward = Vector3.zero;
+
+            // ...
+            if (next != null)
+            {
+                distance = Vector3.Distance(next.position, child.position);
+                forward = next.position - child.position;
+            }
 
             // Set values
             nodes[i].position = child.position;
-            nodes[i].length = dist;
-            length += dist;
+            nodes[i].forward = forward;
+            nodes[i].length = distance;
+            length += distance;
         }
-
-        points = GeneratePoints().ToArray();
     }
     private IEnumerable<_Transform> GeneratePoints()
     {
@@ -87,29 +104,34 @@ public class Conveyor : MonoBehaviour
         float increment = length / amount;
 
         // Generate evenly spaced points (this was an absolute pain)
-        for(int count = 1; count <= amount; count++)
+        for (int count = 1; count <= amount; count++)
         {
-            // No space remains so switch to the next path
-            if(offset > nodes[index].length)
-            {
-                offset -= nodes[index].length;
-                index += 1;
+            // ...
+            var node = nodes[index];
 
+            // No space remains so switch to the next path
+            if (offset > node.length)
+            {
                 // Completed point generation
-                if(nodes[index].isEnd)
+                if (nodes[index + 1].isEnd)
                 {
                     break;
                 }
+
+                // ...
+                offset -= node.length;
+                node = nodes[index + 1];
+                index += 1;
             }
 
-            // Calculate location
-            Vector3 direction = nodes[index + 1].position - nodes[index].position;
-            Vector3 position = nodes[index].position + (offset * direction.normalized);
+            // ...
+            Vector3 position = node.position + (offset * node.forward.normalized);
+            Quaternion rotation = Quaternion.LookRotation(node.forward);
 
             // ...
             offset += increment;
 
-            yield return new _Transform(position, Quaternion.LookRotation(direction));
+            yield return new _Transform(position, node.forward, rotation);
         }
     }
 }
@@ -118,6 +140,8 @@ public class Conveyor : MonoBehaviour
 internal struct Node
 {
     internal Vector3 position;
+    internal Vector3 forward;
+
     internal float length;
     internal bool isEnd
     {
@@ -129,11 +153,13 @@ internal struct Node
 internal struct _Transform
 {
     internal Vector3 position;
+    internal Vector3 forward;
     internal Quaternion rotation;
 
-    internal _Transform(Vector3 pos, Quaternion rot)
+    internal _Transform(Vector3 pos, Vector3 fwd, Quaternion rot)
     {
         position = pos;
+        forward = fwd;
         rotation = rot;
     }
 }
