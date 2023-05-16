93ba645f0ff6bc05c9c71ee0af7b244be9c5345e
Adam <70708407+23calva@users.noreply.github.com>
2023-05-16 08:24:47 -0400

    SAVE

--- Assets/Adam/Scripts/Conveyor/Conveyor.cs
+++ Assets/Adam/Scripts/Conveyor/Conveyor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -12,47 +13,69 @@ public class Conveyor : MonoBehaviour
     public int amount;
     public GameObject clone;
 
+    private float increment;
+
+    private SpawnParts[] partSpawner;
+
     private void Start()
     {
         InitNodes();
-        if(clone == null)
+        if (clone == null)
         {
             Debug.LogError("Provide a reference gameobject in the conveyor script. Inspector -> Debug Mode");
         }
+
+        partSpawner = GameObject.Find("SpawnParts").GetComponents<SpawnParts>();
+
+        StartCoroutine("Move");
     }
-    private void Update()
+    private IEnumerator Move()
     {
+        foreach (var comp in partSpawner)
+        {
+            comp.Spawn();
+        }
+
+        yield return new WaitForSecondsRealtime(5f);
+
+        StartCoroutine("Move");
+
         // Iterate through each point to update transform
-        for(int i = 0; i < points.Length; i++)
+        for (int i = 0; i < points.Length; i++)
         {
             // Helper
-            Node current = nodes[points[i].current];
+            Point point = points[i];
+            Node node = nodes[point.node];
 
             // Increment offset
-            points[i].offset += Time.deltaTime * 0.1f;
+            points[i].offset += increment;
 
             // Calculate position
-            points[i].transform.position = (current.forward * points[i].offset) + current.position;
+            points[i].transform.position = (node.forward * point.offset) + node.position;
 
             // Modify current node
-            if(points[i].offset > current.length)
+            if (point.offset > node.length)
             {
-                int next = points[i].current + 1;
-                points[i].offset -= current.length;
+                int next = points[i].node + 1;
+                points[i].offset -= node.length;
 
-                // SHOWCASE CODE
-                if(nodes[next].isEnd && points[i].transform.childCount > 0)
+                if (nodes[next].isEnd)
                 {
-                    Destroy(points[i].transform.GetChild(0).gameObject);
+                    if (point.transform.childCount != 0)
+                    {
+                        Destroy(point.transform.GetChild(0).gameObject);
+                    }
                 }
 
-                points[i].current = nodes[next].isEnd ? 0 : next;
-                current = nodes[points[i].current];
+                bool end = nodes[next].isEnd;
+
+                points[i].node = end ? 0 : next;
+                node = nodes[point.node];
 
                 // ...
-                points[i].transform.position = current.position;
-                points[i].transform.forward = current.forward;
-                points[i].transform.rotation = current.rotation;
+                points[i].transform.position = node.position;
+                points[i].transform.forward = node.forward;
+                points[i].transform.rotation = node.rotation;
             }
         }
     }
@@ -60,10 +83,10 @@ public class Conveyor : MonoBehaviour
     public void InitNodes()
     {
         // Runtime edits
-        if(Application.isPlaying)
+        if (Application.isPlaying)
         {
             // Destroy the children 😈
-            for(int i = 0; i < transform.childCount; i++)
+            for (int i = 0; i < transform.childCount; i++)
             {
                 Destroy(transform.GetChild(i).gameObject);
             }
@@ -73,10 +96,10 @@ public class Conveyor : MonoBehaviour
         }
 
         // ...
-        for(int i = 0; i < nodes.Length; i++)
+        for (int i = 0; i < nodes.Length; i++)
         {
             // Mark endpoint
-            if(i + 1 >= nodes.Length)
+            if (i + 1 >= nodes.Length)
             {
                 nodes[i].isEnd = true;
                 break;
@@ -98,6 +121,8 @@ public class Conveyor : MonoBehaviour
             beltLength += distance;
         }
 
+        increment = beltLength / amount;
+
         points = GeneratePoints().ToArray();
     }
     private IEnumerable<Point> GeneratePoints()
@@ -105,19 +130,17 @@ public class Conveyor : MonoBehaviour
         int index = 0;
         float offset = 0.0f;
 
-        float increment = beltLength / amount;
-
         // Generate evenly spaced points (this was an absolute pain)
-        for(int count = 1; count <= amount; count++)
+        for (int count = 1; count <= amount; count++)
         {
             // ...
             var node = nodes[index];
 
             // No space remains so switch to the next path
-            if(offset > node.length)
+            if (offset > node.length)
             {
                 // Completed point generation
-                if(nodes[index + 1].isEnd)
+                if (nodes[index + 1].isEnd)
                 {
                     break;
                 }
@@ -130,7 +153,7 @@ public class Conveyor : MonoBehaviour
 
             // ...
             var point = new Point();
-            if(Application.isPlaying)
+            if (Application.isPlaying)
             {
                 point.gameObject = Instantiate(clone, transform);
                 point.gameObject.SetActive(true); // GameObjects spawned disabled?
@@ -141,7 +164,7 @@ public class Conveyor : MonoBehaviour
 
             // ...
             point.offset = offset;
-            point.current = index;
+            point.node = index;
 
             // ...
             offset += increment;
@@ -169,5 +192,5 @@ public struct Point
     public Transform transform => gameObject.transform;
 
     public float offset;
-    public int current;
+    public int node;
 }
--- Assets/Adam/Scripts/SpawnParts.cs
+++ Assets/Adam/Scripts/SpawnParts.cs
@@ -9,17 +9,8 @@ public class SpawnParts : MonoBehaviour
     public GameObject partPrefab;
     public Vector3 position;
 
-    private void Start()
-    {
-        StartCoroutine("Routine");
-    }
-
-    private IEnumerator Routine()
+    public void Spawn()
     {
         Instantiate(partPrefab, position, transform.rotation);
-
-        yield return new WaitForSecondsRealtime(5.0f);
-
-        StartCoroutine("Routine");
     }
 }
